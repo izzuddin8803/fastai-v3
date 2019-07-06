b084473337996e78244a7d713484d49de582da9a
izzuddin <izzuddin8803@gmail.com>
2019-07-06 20:33:14 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/qy8n2hfz2hgeub4/export.pkl?dl=0'
 export_file_name = 'export.pkl'
 
 classes = ['cakoi', 'pisang_goreng', 'kentang_goreng']
